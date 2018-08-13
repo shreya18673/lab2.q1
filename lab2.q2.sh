f1d62ae563c55d5368dc299649ee9ba829445861
shreya18673 <42138883+shreya18673@users.noreply.github.com>
2018-08-13 10:14:39 +0000

    Add files via upload

---
+++ lab2.q2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+mkdir Shreya
+cd Shreya
+touch shreya1.cpp
+touch shreya2.cpp
+touch shreya3.cpp
+touch shreya3.cpp
+touch shreya4.cpp
+touch shreya5.cpp
+echo "What is your first name?"
+read Shreya
+echo "What is your last name?"
+read Sharma
+echo "Your name is $Shreya $Sharma !"
+
