29d799492e16aa9fec24225eab3e6deefef4a562
shreya18673 <42138883+shreya18673@users.noreply.github.com>
2018-08-17 11:39:53 +0000

    Delete lab2.q1.sh

--- lab2.q1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mkdir Shreya
-cd Shreya
-touch shreya.cpp
-echo "What is your first name?"
-read Shreya
-echo "What is your last name?"
-read Sharma
-echo "Your name is $Shreya $Sharma "
